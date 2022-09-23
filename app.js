6bdce4090515e3fdeb308c54b77c59e44cae2b28
sm-shakib <smsakib2403@gmail.com>
2022-09-23 21:51:34 +0600

    add contactRouters.js

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const morgan = require('morgan')
+
+
+const contactRoutes = require("./contactRoutes");
 
 const app = express();
+app.use(morgan("dev"));
+
+app.use("./Contacts", contactRoutes);
+
+app.get("*", (req, res) => {
+  res.send("<h1>Please use the correcte routes</h1>");
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
